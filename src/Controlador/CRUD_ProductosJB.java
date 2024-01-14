8968b91d911760e3dc4a14339d6ca13899af0633
TheBigG7 <127127397+TheBigG7@users.noreply.github.com>
2024-01-14 18:54:14 -0500

    CRUD componentes JB
    introduccion al crud de los componentes

---
+++ src/Controlador/CRUD_ProductosJB.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import Vista.CRUD_Productos_JB;
+
+/**
+ *
+ * @author TheBigG
+ */
+public class CRUD_ProductosJB {
+    private CRUD_Productos_JB vista;
+
+    public CRUD_ProductosJB(CRUD_Productos_JB vista) {
+        this.vista = vista;
+        vista.setVisible(true);
+    }
+
+    public void control(){
+        
+    }
+}
--- src/Main/ProyectoFinal.java
+++ src/Main/ProyectoFinal.java
@@ -1,9 +1,13 @@
 package Main;
 
+import Vista.CRUD_Productos_JB;
+
 public class ProyectoFinal {
     
     public static void main(String[] args) {
+        CRUD_Productos_JB p_jb = new CRUD_Productos_JB();
         
+        p_jb.setVisible(true);
     }
     
 }
