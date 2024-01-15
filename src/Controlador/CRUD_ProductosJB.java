6b7b1c23e3ac49abb8af4520a2bdfeae0695061e
TheBigG7 <127127397+TheBigG7@users.noreply.github.com>
2024-01-15 14:02:50 -0500

    CRUD Componentes all
    habilite el cambio de titulo dependiendo de los botones botones

--- src/Controlador/CRUD_ProductosJB.java
+++ src/Controlador/CRUD_ProductosJB.java
@@ -20,7 +20,16 @@ public class CRUD_ProductosJB {
     }
 
     public void iniciar() {
+        //Botones componentes
         vista.getBtProcesador().addActionListener(l -> crud(1));
+        vista.getBtPlacaMadre().addActionListener(l -> crud(2));
+        vista.getBtRam().addActionListener(l -> crud(3));
+        vista.getBtAlmacenamiento().addActionListener(l -> crud(4));
+        vista.getBtTarjetaGrafica().addActionListener(l -> crud(5));
+        vista.getBtDisipador().addActionListener(l -> crud(6));
+        vista.getBtVentiladores().addActionListener(l -> crud(7));
+        vista.getBtFuente().addActionListener(l -> crud(8));
+        vista.getBtGabinete().addActionListener(l -> crud(9));
         vista.getBtAtrasDlg().addActionListener(l -> atrasDlg());
     }
 
@@ -30,20 +39,28 @@ public class CRUD_ProductosJB {
                 vista.getLblTitulo().setText("Procesadores");
                 break;
             case 2:
+                vista.getLblTitulo().setText("Placas madre");
                 break;
             case 3:
+                vista.getLblTitulo().setText("RAMs");
                 break;
             case 4:
+                vista.getLblTitulo().setText("Almacenamientos");
                 break;
             case 5:
+                vista.getLblTitulo().setText("Tarjetas Graficas");
                 break;
             case 6:
+                vista.getLblTitulo().setText("Disipadores");
                 break;
             case 7:
+                vista.getLblTitulo().setText("Ventiladores");
                 break;
             case 8:
+                vista.getLblTitulo().setText("Fuentes de Poder");
                 break;
             case 9:
+                vista.getLblTitulo().setText("Gabinetes");
                 break;
         }
         vista.setEnabled(false);
@@ -57,6 +74,7 @@ public class CRUD_ProductosJB {
 
     public void atrasDlg() {
         vista.getJdlgCRUD().setVisible(false);
+        vista.getJdlgCRUD().dispose();
         vista.setEnabled(true);
         vista.setVisible(true);
     }
